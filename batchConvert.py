8771443003203bc8f16149fb1429a7cb0e7e579e
Mike iLL Kilmer <mike@mzoo.org>
2015-10-17 19:39:45 -0500

    Add options parser for better command line options.

--- batchConvert.py
+++ batchConvert.py
@@ -1,11 +1,8 @@
-# dupFinder.py
+# batchConvert.py
 import os
 import sys
-from shutil import move
-from datetime import datetime
 import subprocess as sp
-
-usage = 'Usage: python batchConvert.py folder or python [-t] dupFinder.py folder1 folder2 folder3'
+from optparse import OptionParser
 
 FFMPEG_BIN = "ffmpeg" # on Linux ans Mac OS
 # FFMPEG_BIN = "ffmpeg.exe" # on Windows
@@ -30,63 +27,77 @@ def find_aifs(parentFolder):
     return aifs
  
  
-def handle_results(aifs_list, testrun=0):
+def handle_results(aifs_list, options):
 		"""
-		Get the FULL list of all found aif files and make mp3 versions using ffmpeg
+		Get the FULL list of all found aif files and make wav versions using ffmpeg
 		"""
-		print("There are {} aif files, first of which is {}, last is {}".format(len(aifs_list), aifs_list[0] \
-																																			, aifs_list[-1]))
 		
-
+		if options.examine == True:
+				print("There are {} aif files, first of which is {}, last is {}".format(len(aifs_list), aifs_list[0] \
+																																			, aifs_list[-1]))
+				sys.exit()
+		
 		for aif in aifs_list:		
 				basename = os.path.basename(aif)
 				basefilename, file_extension = os.path.splitext(basename)
-				save_to = os.path.dirname(aif) + "/" + basefilename + '.wav'
+				save_to = os.path.dirname(aif) + "/" + basefilename + '.' + options.format
+				print(save_to)
 				if not os.path.exists(save_to):
-						command_not = [ FFMPEG_BIN,
+						command_mp3 = [ FFMPEG_BIN,
 												'-i', str(aif),
 												'-f', 'mp3',
 												'-acodec', 'libmp3lame',
 												'-ab', str(192000),
-												'-ar', str(441000), save_to
+												'-ar', str(44100), save_to
 												]
-						command = [ FFMPEG_BIN,
+						command_wav = [ FFMPEG_BIN,
 											 '-i', str(aif),  
 											 save_to
 											 ]
-						another_command = [ FFMPEG_BIN,
-														'-i', str(aif),  
-														'-ac', str(1),
-														'-ab', str(64000),
-														'-ar', str(22050),
-														str(save_to)
-														]
+											 
 						print("Creating file {}".format(save_to))
-						pipe = sp.Popen(another_command, stdout = sp.PIPE, bufsize=10**8)
+						if not options.testing == True:
+								if options.format == 'wav':
+										pipe = sp.Popen(command_wav, stdout = sp.PIPE, bufsize=10**8)
+								else:
+										pipe = sp.Popen(command_mp3, stdout = sp.PIPE, bufsize=10**8)
+						else:
+								if options.format == 'wav':
+										print("Testing: {}".format(command_wav))
+								else:
+										print("Testing: {}".format(command_mp3))
 
- 
+def get_options(warn=False):
+    usage = "usage: %s [options] directory_containing_aif_files" % sys.argv[0]
+    parser = OptionParser(usage=usage)
+    parser.add_option("-t", "--test", action="store_true", dest="testing", help="Don't actually create the new files.")
+    parser.add_option("-e", "--examine", action="store_true", dest="examine", help="Count number of files in specified dirs.")
+    parser.add_option("-f", "--format", default='mp3', help="output format default=mp3 - other option is 'wav'")
+    
+    (options, args) = parser.parse_args()
+    if warn and len(args) < 1: 
+        parser.print_help()
+    return (options, args)
+     
 def main(args): 
-    """
-    Do the work of the program if args provided, or exit and show usage.
-    """
-    if not len(args) > 1:
-        print(usage)
-        sys.exit()
-    if args[1] == '-t':
-        testrun = 1
-        directories = args[2:]
-    else:
-        testrun = 0
-        directories = args[1:]
-    for dir in directories:
-        # Iterate the folders given
-        if os.path.exists(dir):
-            # Find the duplicated files and append them to the dups
-            all_aifs = find_aifs(dir)
-        else:
-            print('\'%s\' is not a valid path, please verify' % dir)
-            sys.exit()
-    handle_results(all_aifs, testrun)
+		"""
+		Do the work of the program if args provided, or exit and show usage.
+		"""
+		options, args = get_options(warn=True)
+		aifs = []
+		print(options)
+		print(args)
+		for dir in args:
+				# Iterate the folders given
+				if os.path.exists(dir):
+						# strip trailing slash if exists
+						dir = dir.rstrip('/')
+						# Find the aif files and extend aifs list
+						aifs.extend(find_aifs(dir))
+				else:
+						print('\'%s\' is not a valid path, please verify' % dir)
+						sys.exit()
+		handle_results(aifs, options)
 
 if __name__ == '__main__':
     main(sys.argv)
